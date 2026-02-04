91175c39e998bbc2e4d57018821adb1a43591b41
AllAimNoMental <luis.colmenares@students.snow.edu>
2026-02-03 20:44:34 -0700

    Part1

--- FInalBattler/Character/Hero.cs
+++ FInalBattler/Character/Hero.cs
@@ -5,6 +5,7 @@ namespace FinalBattler.Character
 {
     public class Hero : Creations, IHero
     {
+
         public int Health { get; set; }
         public int Power { get; set; }
         public int Luck { get; set; }
@@ -18,6 +19,50 @@ namespace FinalBattler.Character
 
         public Hero()
         {
+            Name = "Unknown";
+            Level = 0;
+            TotalHealth = 1;
+            TotalLuck = 1;
+            TotalPower = 1;
+
         }
+
+        public void LevelUp()
+        {
+            Random random = new Random();
+            switch (CombatClass)
+            {
+                case CombatClass.None:
+                    break;
+
+                case CombatClass.Warrior:
+                    Health += random.Next(10, 21);
+                    Power += random.Next(1, 4);
+                    Luck += random.Next(1, 4);
+                    break;
+
+                case CombatClass.Wizard:
+                    Health += random.Next(1, 16);
+                    Power += random.Next(3, 6);
+                    Luck += random.Next(1, 4);
+                    break;
+
+                case CombatClass.Rogue:
+                    Health += random.Next(1, 16);
+                    Power += random.Next(1, 4);
+                    Luck += random.Next(3, 6);
+                    break;
+            }
+        }
+
+        public void DisplayStats(bool showTotalStats = false)
+        {
+
+        }
+       public  void CalculateTotals()
+        {
+
+        }
+
     }
 }
